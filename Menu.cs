500213a667ec64480726ca404f3e80b1873fd8a8
Liudmyla Rybak <rybakl266@gmail.com>
2020-07-01 17:08:08 +0300

    Upgraded menu

--- Menu.cs
+++ Menu.cs
@@ -3,37 +3,50 @@ namespace Vigenere
 {
     static class Menu
     {
-        delegate void MessageHandler(string message);
         static event MessageHandler ShowMessage;
-        static Menu() => ShowMessage += Display; 
+        static Menu() => ShowMessage += Display;
         public static void ChooseProcedure()
         {
-            ShowMessage.Invoke("--> Choose encryption(0) or decryption(1) <--");
-            int procedure = Convert.ToInt32(Console.ReadLine());
-            ShowMessage.Invoke("--> Enter your text to process <--");
-            string text = Console.ReadLine();
-            ShowMessage.Invoke("--> Enter tour key <--");
-            string key = Console.ReadLine();
-            DataProcessing(procedure, text, key);
+            string procedure = null;
+            while (procedure != "exit")
+            {
+                ShowMessage.Invoke("--> Choose encryption or decryption <--");
+                ShowMessage.Invoke("--> Enter exit to stop the program");
+                procedure = Console.ReadLine();
+                DataProcessing(procedure);
+            }
         }
-        static void DataProcessing(int procedure, string text, string key)
+        static void DataProcessing(string procedure)
         {
-            switch(procedure)
-            { 
-                case 0:
+            string text = null, key = null;
+            switch (procedure)
+            {
+                case "encryption":
+                    GetText(ref text, ref key);
                     VigenereEncryptionService encryptedMessage = new VigenereEncryptionService(text);
                     ShowMessage.Invoke("*** Encrypted text ***");
                     encryptedMessage.MessageEncrypt(key);
                     break;
-                case 1:
+                case "decryption":
+                    GetText(ref text, ref key);
                     VigenereDecryptionService decryptedMessage = new VigenereDecryptionService(text);
                     ShowMessage.Invoke("*** Deccrypted text ***");
                     decryptedMessage.MessageDecrypt(key);
+                    break;
+                case "exit":
+                    ShowMessage.Invoke("Have a nice day!");
                     break;
                 default:
                     ShowMessage.Invoke("There`s no suitable operation");
                     break;
             }
+        }
+        static void GetText(ref string text, ref string key)
+        {
+            ShowMessage.Invoke("--> Enter your text to process <--");
+            text = Console.ReadLine();
+            ShowMessage.Invoke("--> Enter your key <--");
+            key = Console.ReadLine();
         }
         static void Display(string message) => Console.WriteLine(message);
     }
--- VigenereProcedureService.cs
+++ VigenereProcedureService.cs
@@ -2,16 +2,16 @@
 using System.Linq;
 namespace Vigenere
 {
+    delegate void MessageHandler(string message);
     static class VigenereProcedureService
     {
+        static event MessageHandler ShowMessage;
         public static bool KeyCheck(string key)
         {
             if (!key.All(char.IsLetter))
                 return false;
             return true;
         }
-        delegate void MessageHandler(string message);
-        static event MessageHandler ShowMessage;
         static bool checkSubscription = false;
         static void DisplayMessage(string message) => Console.WriteLine(message);
         public static void InvokeMessage(string message)
